1b1c68553e3a12b13ce7f3121c947d2771842547
Jacob Jarecki <jsjwooowoo@gmail.com>
2014-11-06 22:28:53 -0800

    Usher now at point to read single peripheral's data once format specified

--- UsherApp/UsherApp/UsherApp/ViewController.swift
+++ UsherApp/UsherApp/UsherApp/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import CoreBluetooth
 import QuartzCore
 
-class ViewController: UIViewController, CBCentralManagerDelegate {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     
     let TM_USHER_CLIENT_COMMS = CBUUID(string: "BA0A5518-4D18-443A-817F-D726062085BD")
+    let TM_USHER_CLIENT_COMMS_CHARACTERICS = CBUUID(string: "C04761A0-51E9-4653-B51D-FC93C3B691CF")
     
     var ticketNumber : Int = 0
     
@@ -34,7 +35,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    ////			 UI Functions
+    //// UI Functions
     
     @IBAction func startScan(sender: AnyObject){
         if (scanSwitch.on){
@@ -57,16 +58,42 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     
     //// CBDelegateManager
     
-    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        centralManager.connectPeripheral(peripheral, options: nil)
     }
     
-    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        peripheral.delegate = self
+        println("Connected to discovered peripheral: \(peripheral.name)")
+        // May not be needed since we know what service to expect here
+        peripheral.discoverServices([TM_USHER_CLIENT_COMMS])
     }
     
     func centralManagerDidUpdateState(central: CBCentralManager!) {
     }
     
     
+    /// CBPeripheralDelegate
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        for service in peripheral.services {
+            peripheral.discoverCharacteristics([TM_USHER_CLIENT_COMMS_CHARACTERICS], forService: service as CBService)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        for characteristic in service.characteristics {
+            peripheral.readValueForCharacteristic(characteristic as CBCharacteristic)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!){
+        if characteristic.properties == CBCharacteristicProperties.Read {
+            // Convert characteristic.value to an integer to set ticketNumber
+            self.ticketNumber = 0
+            refreshUI()
+        }
+    }
     
     
     
