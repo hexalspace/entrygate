5a6aae1b27489d1a1d6dba4019bbd6af8178d759
Rbdall <rbdall@ucla.edu>
2014-11-24 11:01:06 -0800

    Recreated FanApp project - now runs on hardware

--- FanApp/FanAppTests/FanAppTests.swift
+++ FanApp/FanAppTests/FanAppTests.swift
@@ -2,7 +2,7 @@
 //  FanAppTests.swift
 //  FanAppTests
 //
-//  Created by Ryan Dall on 11/6/14.
+//  Created by Ryan Dall on 11/24/14.
 //  Copyright (c) 2014 TM-UCLA-BLE. All rights reserved.
 //
 
